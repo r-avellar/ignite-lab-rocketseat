a159d6bbf9e662270a66493bb8e1899f32844f0d
r-avellar <renatoavellar5@gmail.com>
2022-06-23 22:01:45 -0300

    aula 03 roteamento das paginas e abrindo o video das aulas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { Router } from "./Router"
+import { client } from "./lib/apollo"
+import { BrowserRouter } from "react-router-dom"
 
 
 function App() {
 return(
-   <Event />
+   <ApolloProvider client={client}>
+      <BrowserRouter>
+         <Router />
+      </BrowserRouter>
+      
+   </ApolloProvider>
+   
 )
 }
 

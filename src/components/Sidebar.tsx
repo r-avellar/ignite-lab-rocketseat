eb4c8a8db9a48698d5ca149a7a8eb4fbbc6050b0
r-avellar <renatoavellar5@gmail.com>
2022-06-24 23:49:14 -0300

    aula 05 finalizando aplicação

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-query {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}
-`
-
-interface GetLessonsQueryResponse{
-    lessons:{
-        id: string
-        title: string
-        slug: string
-        availableAt: string
-        lessonType: 'live' | 'class' 
-    }[]
-}
 
 export function Sidebar(){
 
-    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+    const { data } = useGetLessonsQuery()
 
     console.log(data)
 
@@ -40,7 +19,7 @@ export function Sidebar(){
                         <Lesson
                             key={lesson.id}
                             title={lesson.title}
-                            slug={lesson.slug}
+                            slug="slug"//{lesson.slug}
                             availableAt={new Date(lesson.availableAt)}
                             type={lesson.lessonType}
                 />
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,36 +2,7 @@ import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
-import { gql, useQuery } from "@apollo/client";
-
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-query getLessonBySlug( $slug: String) {
-  lesson(where: {slug: $slug}) {
-    videoId
-    title
-    description
-    teacher {
-      bio
-      avatarURL
-      name
-    }
-  }
-}
-`
-
-interface GetLessonBySlugResponse{
-    lesson:{
-        title: string;
-        videoId: string;
-        description: string;
-        teacher:{
-            bio: string;
-            avatarURL: string;
-            name: string;
-        }
-    }
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 
 interface VideoProps{
@@ -39,14 +10,14 @@ interface VideoProps{
 }
 
 export function Video(props: VideoProps){
-    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    const { data } = useGetLessonBySlugQuery({
         variables:{
             slug: props.lessonSlug,
         }
     }
      )
 
-    if(!data){
+    if(!data || !data.lesson){
         return(
             <div className="flex-1">
                 <p>Carregando....</p>
@@ -75,7 +46,8 @@ export function Video(props: VideoProps){
                             {data.lesson.description}
                         </p>
 
-                        <div className="flex items-center gap-4 mt-6">
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
                             <img 
                             className="h-16 w-16 rounded-full border-2 border-blue-500"
                             src={data.lesson.teacher.avatarURL}
@@ -86,6 +58,8 @@ export function Video(props: VideoProps){
                                 <span className="tex-gray-200 text-sm block">CTO @Rocketseat</span>
                             </div>
                         </div>
+                        )}
+                        
                     </div>
 
                     <div className="flex flex-col gap-4">
